9c98d66dc4dfbb3ec8fbc6919970e005f218500d
topHRio <abdusaid@tophr.io>
2025-08-05 16:18:48 +0500

    Update app.py

--- app.py
+++ app.py
@@ -293,3 +293,7 @@ def get_statement(_id, params):
         json.dumps(response_obj, separators=(",", ":")),
         mimetype="application/json"
     )
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
